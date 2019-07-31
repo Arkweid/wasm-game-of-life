d75f02911beca5c48784e6ac755753aae01bf3a6
Alexander Abroskin <arkweid@evilmartians.com>
2019-07-31 22:39:33 +0300

    render game in js

--- www/index.js
+++ www/index.js
@@ -1,3 +1,16 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet("Alex");
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+  setTimeout(func, 1000);
+};
+
+requestAnimationFrame(renderLoop);
+
+function func() {
+    requestAnimationFrame(renderLoop);
+}
